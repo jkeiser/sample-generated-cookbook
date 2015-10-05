c00c04a91b7d64f6cf81e1a809400e828067f945
John Keiser <john@johnkeiser.com>
2015-10-05 12:20:09 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'blah'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures blah'
+long_description 'Installs/Configures blah'
+version '0.1.0'
